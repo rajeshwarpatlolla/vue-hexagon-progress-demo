01eec5b287173f5a8ff0b13c66d974271ec80026
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2018-04-06 13:18:29 +0530

    added hexagon component

---
+++ src/components/vue-hexagon-demo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>Hexagon Graph Demo</div>
+    <div class="pt-20">
+      <hexagonGraph :options="{width:120, height:120, trackColor:'#bb28bf', title:'Avg', label:'70%', valueColor:'#000', value:70, className:'containment-hexagon'}" />
+      </div>
+  </div>
+</template>
+
+<script>
+import hexagonGraph from './vue-hexagon';
+
+export default {
+  name: 'VueHexagonDemo',
+  data() {
+    return {};
+  },
+  components: { hexagonGraph },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.pt-20{
+  padding-top:20px;
+}
+</style>
